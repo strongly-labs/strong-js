ac4d49a082ea30f95e530fac49e9724f0c9927d3
protagonist <hi.zuhair@gmail.com>
2021-07-22 16:36:08 +1000

    refactored

--- packages/generate/src/index.ts
+++ packages/generate/src/index.ts
@@ -1,3 +1,18 @@
 #!/usr/bin/env node
 
-import './generate'
+import { generatorHandler } from '@prisma/generator-helper'
+import generateApi from './api'
+import generateSchema from './schema'
+
+generatorHandler({
+  onManifest() {
+    return {
+      defaultOutput: '.strongly',
+      prettyName: 'Prisma Strongly Generator',
+    }
+  },
+  async onGenerate(options) {
+    await generateApi(options)
+    await generateSchema(options)
+  },
+})
