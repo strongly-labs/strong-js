4a811bd6133bcf484ff6dfd7fbbc4aef70833932
protagonist <hi.zuhair@gmail.com>
2021-07-25 13:34:36 +1000

    wip

--- apps/web/admin/strongly-link.js
+++ apps/web/admin/strongly-link.js
@@ -1,10 +1,43 @@
 const fs = require('fs').promises
+const constants = require('fs').constants
+
+const { access, realpath, symlink } = fs
+
+async function linkStrongly(stronglyPath) {
+  try {
+    await access('./.strongly', constants.R_OK)
+    console.log('.strongly dir alrealdy linked')
+  } catch (handledError) {
+    try {
+      await symlink(stronglyPath, './.strongly', 'junction')
+      console.log('linked .strongly dir')
+    } catch (unhandledError) {
+      throw unhandledError
+    }
+  }
+}
+
+async function linkApi(stronglyPath) {
+  const apiPath = './pages/api/s'
+  try {
+    await access(apiPath, constants.F_OK)
+    console.log('strongly api alrealdy linked')
+  } catch (handledError) {
+    try {
+      await symlink(stronglyPath + '/api', apiPath, 'junction')
+      console.log('linked strongly api')
+    } catch (unhandledError) {
+      throw unhandledError
+    }
+  }
+}
 
 async function createLinks() {
   try {
-    const target = await fs.realpath('../../../.strongly')
+    const stronglyPath = await realpath('../../../.strongly')
 
-    await fs.symlink(target, './.strongly', 'junction')
+    await linkStrongly(stronglyPath)
+    await linkApi(stronglyPath)
   } catch (error) {
     throw error
   }
