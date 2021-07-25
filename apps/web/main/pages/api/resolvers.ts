0059c02a8dd77ead659ef6ea23fd5f8649b40689
Zuhair Naqvi <hi.zuhair@gmail.com>
2021-07-25 17:59:13 +1000

    graphql added

---
+++ apps/web/main/pages/api/resolvers.ts
@@ -0,0 +1,32 @@
+import {
+  UserRelationsResolver,
+  UserCrudResolver,
+  ResolversEnhanceMap,
+  ResolverActionsConfig,
+  applyResolversEnhanceMap,
+  Role,
+} from '../../../admin/.strongly/graphql'
+import { Authorized, Extensions } from 'type-graphql'
+
+// define the decorators config using generic ResolverActionsConfig<TModelName> type
+const userActionsConfig: ResolverActionsConfig<'User'> = {
+  users: [Authorized(Role.ADMIN)],
+  updateUser: [Authorized(Role.ADMIN)],
+  deleteUser: [
+    Authorized(Role.ADMIN),
+    Extensions({ logMessage: 'Danger zone' }),
+  ],
+}
+
+// join the actions config into a single resolvers enhance object
+const resolversEnhanceMap: ResolversEnhanceMap = {
+  User: userActionsConfig,
+}
+
+// apply the config (it will apply decorators on the resolver class methods)
+applyResolversEnhanceMap(resolversEnhanceMap)
+
+export default {
+  UserRelationsResolver,
+  UserCrudResolver,
+}
