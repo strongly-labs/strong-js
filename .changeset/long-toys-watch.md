fdd5c8bd623123c41ac21b52779f092d8863ce49
Zuhair Naqvi <hi.zuhair@gmail.com>
2021-07-30 22:33:37 +1000

    init fixed

---
+++ .changeset/long-toys-watch.md
@@ -0,0 +1,5 @@
+---
+'@strong-js/cli': patch
+---
+
+Clone from git template on init and yarn install
--- packages/cli/src/commands/init.ts
+++ packages/cli/src/commands/init.ts
@@ -1,5 +1,6 @@
 import chalk from 'chalk'
 import ora from 'ora'
+import execa from 'execa'
 
 import {
   uniqueNamesGenerator,
@@ -7,7 +8,6 @@ import {
   adjectives,
   animals,
 } from 'unique-names-generator'
-import { createProject } from '../lib'
 
 const customConfig: Config = {
   dictionaries: [adjectives, animals],
@@ -30,10 +30,18 @@ const init = async () => {
   try {
     const name = await namePrompt.run()
     spinner.start(`Creating ${chalk.blue.bold(name)} `)
-    const success = await createProject(name)
-    if (success) {
-      spinner.succeed(`Package created successfully`)
-    }
+
+    await execa('git', [
+      'clone',
+      'git@github.com:strongly-labs/template-strong-project.git',
+      name,
+    ])
+
+    process.chdir(name)
+
+    await execa('yarn', ['install'])
+
+    spinner.succeed(`Package created successfully`)
   } catch (error) {
     spinner.fail('Failed to create package')
     throw error
